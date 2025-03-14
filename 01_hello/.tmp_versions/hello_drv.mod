3b61c48bc4810d3decba280c707eb4d2aa5179e4
cgs <2516166076@qq.com>
2025-03-14 03:16:47 -0400

    提交第一个驱动程序hello

---
+++ 01_hello/.tmp_versions/hello_drv.mod
@@ -0,0 +1,3 @@
+/home/book/nfs_rootfs/code/01_hello/hello_drv.ko
+/home/book/nfs_rootfs/code/01_hello/hello_drv.o
+
---
+++ 01_hello/hello_test.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+/*
+./test /dev/xxx hello  写
+./test /dev/xxx        读	
+*/
+int main(int argc, char **argv)
+{
+	int fd;
+	int len;
+	char buf[100];
+	
+	if(argc < 2)
+	{
+		printf("usage %s <dev> [string]",argv[0]);
+		return -1;
+	}
+	
+	fd = open(argv[1], O_RDWR);
+	if(fd < 0 )
+	{
+		printf("open %s fail\n",argv[1]);
+		return -1;
+	}
+
+	if(argc == 3)
+	{
+		len = write(fd, argv[2], strlen(argv[2])+1);
+		printf("write ret = %d\n",len );
+	}
+	else
+	{
+		len = read(fd, buf, 100);
+		buf[99] = '\n';
+		printf("read ret = %d\n",len );
+	}
+
+	return 0;
+}
