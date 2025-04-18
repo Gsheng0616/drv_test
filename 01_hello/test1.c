2602e6b77a8852ff0b3a0ebb7fe6b632ad916c32
cgs <2516166076@qq.com>
2025-04-18 06:53:44 -0400

    111

--- 01_hello/test1.c
+++ 01_hello/test1.c
@@ -69,3 +69,27 @@ int bubble_sort(int *a, int len)
                         
             }
 }
+
+int quick_sort(int s[], int l, int r)   
+{
+    if (l < r)
+    {
+        int i = l, j = r, x = s[l];
+        while (i < j)
+        {
+            while(i < j && s[j] >= x)
+                j--;
+            if(i < j)
+                s[i++] = s[j];
+            while(i < j && s[i] < x)
+                i++;
+            if(i < j)
+                s[j--] = s[i];
+        }
+        s[i] = x;
+        quick_sort(s, l, i - 1);
+        quick_sort(s, i + 1, r);
+
+    }
+    
+}
