0865df45806634e16bb64aa29d58d9342120103a
cgs <2516166076@qq.com>
2025-04-18 05:10:37 -0400

    测试

--- 01_hello/test1.c
+++ 01_hello/test1.c
@@ -31,12 +31,39 @@ void func()
 //
 //
 
-int main()  
-{
-    int a = 10;
-    int *p = &a;
-    printf("address [0x%p]\r\n", p);
-    *p = 20;
-    printf("a = %d\r\n", a);
+// 冒泡排序函数
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // 交换元素
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// 打印数组函数
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("原始数组: ");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("排序后的数组: ");
+    printArray(arr, n);
+    
     return 0;
 }
